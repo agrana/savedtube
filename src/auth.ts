a19b0de750099bc2f5f24a72608b9ca7d699dea1
Alfonso Graña <alfonso.grana@gmail.com>
2025-08-19 20:27:10 +0200

    feat: add global nextauth configuration

---
+++ src/auth.ts
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import NextAuth from 'next-auth'
+import Google from 'next-auth/providers/google'
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          scope: 'openid email profile https://www.googleapis.com/auth/youtube.readonly',
+          access_type: 'offline',
+          prompt: 'consent',
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account, profile }: { token: any; account: any; profile: any }) {
+      if (account) {
+        token.accessToken = account.access_token
+        token.refreshToken = account.refresh_token
+        token.expiresAt = account.expires_at
+      }
+      if (profile && 'picture' in profile) {
+        token.picture = profile.picture as string
+      }
+      return token
+    },
+    async session({ session, token }: { session: any; token: any }) {
+      session.accessToken = token.accessToken
+      session.user.id = token.sub!
+      return session
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: process.env.NODE_ENV === 'development',
+})
